8e3d5a289bb98b9720b8764fe51de5cacbfe52fa
Luciano Bosco <lucianobosco@gmail.com>
2023-02-01 14:03:04 +0100

    Refactor index.php for DI and Controllers

--- public/index.php
+++ public/index.php
@@ -1,12 +1,21 @@
 <?php
+
+use App\Context\Country\Infrastructure\CountryController;
+use DI\Container;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
+use DI\Bridge\Slim\Bridge as SlimAppFactory;
 
 require __DIR__ . '/../vendor/autoload.php';
 
 $app = AppFactory::create();
 
+// Create a new container for dependency injection
+$container = new Container();
+$app = SlimAppFactory::create($container);
+
+$app->get('/country-check/{code}', [CountryController::class, 'checkCountry']);
 $app->get('/', function (Request $request, Response $response, $args) {
     $response->getBody()->write("Hello world!");
     return $response;
